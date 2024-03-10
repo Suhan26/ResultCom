c00ccb8d8711bc7ed71a52bf9d977758695a9ff5
suhan <md.suhanurrohanur2024@gmail.com>
2024-03-10 21:52:50 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+var my_score = 88 ;
+
+var my_frand =57 ;
+
+if (my_score > 80) {
+    if (my_frand >= 80 ) {
+        console.log("Go for Lanch");
+    }
+    
+    else if (my_frand < 80 && my_frand > 60) {
+        console.log("good luck mext time");
+    }
+    else if (my_frand < 60 && my_frand > 40) {
+        console.log("Not seen");
+    }
+    
+    else if (my_frand > 40) {
+        console.log("unFrand");
+    }
+    
+}
+else{console.log("not Comparigone");}
+
+
+
+
+
